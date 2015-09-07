eaa6e02bdd86b696e45a863afac18496c2b28063
intrd <x@dann.com.br>
2015-09-07 16:27:02 -0300

    license

--- functions.php
+++ functions.php
@@ -6,9 +6,9 @@
 * @version 1.0
 * @category functions
 * @author intrd - http://dann.com.br/
-* @link https://github.com/intrd/php-common/
 * @copyright 2015 intrd
 * @license http://creativecommons.org/licenses/by/4.0/
+* @link https://github.com/intrd/php-common/
 *
 */
 
