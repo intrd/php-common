1c5efdbb709d7a834358a311dc5163853f24b38b
intrd <x@dann.com.br>
2015-12-16 13:43:21 -0200

    array sort by column

--- functions.php
+++ functions.php
@@ -263,4 +263,13 @@ function overtrim($string,$qty){
     return $string;
   }
 }
+
+function array_sort_by_column(&$arr, $col, $dir = SORT_ASC) {
+    $sort_col = array();
+    foreach ($arr as $key=> $row) {
+        $sort_col[$key] = $row[$col];
+    }
+
+    array_multisort($sort_col, $dir, $arr);
+}
 ?>
